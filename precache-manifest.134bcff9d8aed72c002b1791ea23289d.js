cb93572c9392e0f55421c4bed5541f3aea5a6f1e
Raed Salah <Raed-Salah@github.com>
2020-10-01 05:31:38 +0200

    Updates

---
+++ precache-manifest.134bcff9d8aed72c002b1791ea23289d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91beb49671df35b41a255d1609d061d",
+    "url": "/React-TodoList-App/index.html"
+  },
+  {
+    "revision": "852ef76c00ef60b82a41",
+    "url": "/React-TodoList-App/static/css/main.4fbee4a2.chunk.css"
+  },
+  {
+    "revision": "86213eaea5f3a552a1d4",
+    "url": "/React-TodoList-App/static/js/2.d72a66f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-TodoList-App/static/js/2.d72a66f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852ef76c00ef60b82a41",
+    "url": "/React-TodoList-App/static/js/main.744b20fa.chunk.js"
+  },
+  {
+    "revision": "c211f0aa8710a7562d89",
+    "url": "/React-TodoList-App/static/js/runtime-main.c95894a1.js"
+  }
+]);
